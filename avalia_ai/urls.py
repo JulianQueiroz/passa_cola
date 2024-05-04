85a1e880a0b186f5e581395ad3535fcbad21349b
juliana <julianatko@gmail.com>
2024-05-04 03:06:11 -0300

    autenticaçao

--- avalia_ai/urls.py
+++ avalia_ai/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from avaliacao_materias import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('login/', views.login, name='login'),
+    path('cadastro/',views.cadastro,name='cadastro'),
 
 ]
--- avaliacao_materias/views.py
+++ avaliacao_materias/views.py
@@ -1,5 +1,46 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.shortcuts import redirect,render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
+    return render(request,'home.html')
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'auth/cadastro.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = User.objects.filter(username=username).first()
+        if user:
+            return HttpResponse('Já existe um usuário com esse username')
+
+        user = User.objects.create_user(username=username,email=email,password=senha)
+        user.save()
+        return HttpResponse('Cadastrado com sucesso')
+
+def login(request):
+    if request.method == "GET":
+        return render(request,'auth/login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username,password=senha)
+
+        if user:
+            login_django(request,user)
+            return render(request,'home.html')
+        else:
+            return HttpResponse('Email ou senha inválidos')
+
+@login_required(login_url="/auth/login/")
+def plataforma(request):
     return render(request,'home.html')
