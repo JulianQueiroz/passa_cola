23b8fc584dfeba56cb3ec6fe83276e3161da5543
juliana <julianatko@gmail.com>
2024-05-11 21:15:19 -0300

    responsividade, limitação de caracteres e botao 'ver mais'

--- avaliacao_materias/models.py
+++ avaliacao_materias/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Avaliacao(models.Model):
     materia = models.CharField(max_length=100)
-    avaliacao = models.CharField(max_length=100)
+    avaliacao = models.CharField(max_length=3000)
 
     def __str__(self):
         return f"{self.materia},{self.avaliacao}"
