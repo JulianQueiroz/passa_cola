cdb0f17ecf24ca89d54da1495b13e58621f10990
juliana <julianatko@gmail.com>
2024-06-28 16:02:34 -0300

    Ajustar build_files.sh para configurar o ambiente virtual Python

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,20 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Instalar virtualenv se não estiver instalado
+python3 -m pip install --user virtualenv
+
+# Criar um ambiente virtual
+python3 -m venv venv
+
+# Ativar o ambiente virtual
+source venv/bin/activate
+
+# Instalar as dependências do projeto
 pip install -r requirements.txt
 
-# Convert static asset files
-python3 manage.py collectstatic 
+# Converter arquivos estáticos
+python3 manage.py collectstatic --noinput
 
-# Apply any outstanding database migrations
+# Aplicar migrações do banco de dados
 python3 manage.py migrate
